d71f4ae6f7a49946869f248e384993c1fc79fd5b
krushna jadhav <jadhavkrishna475@gmail.com>
2026-03-28 01:24:05 +0000

    added placeholder App component to verify build works

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-dvh items-center justify-center">
+      <h1 className="text-4xl font-bold text-dark-100">PixaSearch</h1>
+    </div>
+  )
+}
+
+export default App
